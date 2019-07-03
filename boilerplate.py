fffa17070fa28ec034ce8aa0f5862e668be7188d
Rahul Gautam <arr.gee23@gmail.com>
2019-07-03 20:47:08 +0530

    update

--- boilerplate.py
+++ boilerplate.py
@@ -1,17 +1,20 @@
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import os
+import random
 
     
-def trainTestSplit(df,testCol,testSize=0.2):
+def trainTestSplit(df,testCol,testSize=0.2,seed=42):
     """having passed a dataframe and a output col splits into 
         train and test set
     """
-    y=data[testCol]
-    x=data.drop(testCol,axis=1)
+    df = df.sample(frac=1).reset_index(drop=True)
+    y=df[testCol]
+    x=df.drop(testCol,axis=1)
     x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=testSize)
     return x_train,x_test,y_train,y_test
 
@@ -20,7 +23,6 @@ def readFileMakeDF(fileName):
     df = pd.DataFrame(data);
     return df
 
-importall()
 df = readFileMakeDF("../input/train.csv")
 x_train,x_test,y_train,y_test = trainTestSplit(df,'Survived')
 x_train
