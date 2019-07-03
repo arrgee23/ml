368d1cc5c55dd666a1cfea15f8e74cce406c3276
Rahul Gautam <arr.gee23@gmail.com>
2019-07-03 23:23:17 +0530

    update

--- boilerplate.py
+++ boilerplate.py
@@ -1,32 +1,3 @@
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-import seaborn as sns
-import os
-import random
-
-    
-def trainTestSplit(df,testCol,testSize=0.2,seed=42):
-    """having passed a dataframe and a output col splits into 
-        train and test set
-    """
-    df = df.sample(frac=1).reset_index(drop=True)
-    y=df[testCol]
-    x=df.drop(testCol,axis=1)
-    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=testSize)
-    return x_train,x_test,y_train,y_test
-
-def readFileMakeDF(fileName):
-    data=pd.read_csv(fileName);
-    df = pd.DataFrame(data);
-    return df
-
-df = readFileMakeDF("../input/train.csv")
-x_train,x_test,y_train,y_test = trainTestSplit(df,'Survived')
-x_train
-
 """
 # Some features of my own that I have added in
 # Gives the length of the name
@@ -103,4 +74,39 @@ for dataset in full_data:
     dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
     dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
     dataset.loc[ dataset['Age'] > 64, 'Age'] = 4 ;
+    
+    # Feature selection
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
+train = train.drop(drop_elements, axis = 1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
+test  = test.drop(drop_elements, axis = 1)
 """
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import os
+import random
+
+    
+def trainTestSplit(df,testCol,testSize=0.2,seed=42):
+    """having passed a dataframe and a output col splits into 
+        train and test set
+    """
+    df = df.sample(frac=1).reset_index(drop=True)
+    y=df[testCol]
+    x=df.drop(testCol,axis=1)
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=testSize)
+    return x_train,x_test,y_train,y_test
+
+def readFileMakeDF(fileName):
+    data=pd.read_csv(fileName);
+    df = pd.DataFrame(data);
+    return df
+
+df = readFileMakeDF("../input/train.csv")
+x_train,x_test,y_train,y_test = trainTestSplit(df,'Survived')
+x_train
+
